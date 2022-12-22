9b3588ebe3f0ec14df2045063dc2951ae99293e4
odevpedro <pedrosschmidt2@gmail.com>
2022-12-22 16:58:19 -0300

    a

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 #!/bin/bash
 
 
-
 #useful tools
 sudo apt install -y htop
 sudo apt install -y tmux
@@ -11,6 +10,22 @@ sudo apt install -y wget
 sudo apt install -y tree
 sudo apt install -y zip
 sudo apt install -y unzip
+sudo apt install -y git
 
 
+#installing asdf 
 
+mkdir version-control
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+
+#asdf configuration
+
+. $HOME/.asdf/asdf.sh
+
+. $HOME/.asdf/completions/asdf.bash
+
+
+#asdf main plugins install
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs latest
