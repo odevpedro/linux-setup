a62ee886648f8e340351bbaf55e7a60adcfd45ab
odevpedro <pedrosschmidt2@gmail.com>
2022-12-22 16:43:55 -0300

    a

---
+++ install.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+
+
+#useful tools
+sudo apt install -y htop
+sudo apt install -y tmux
+sudo apt install -y curl
+sudo apt install -y wget
+sudo apt install -y tree
+sudo apt install -y zip
+sudo apt install -y unzip
+
+
+
